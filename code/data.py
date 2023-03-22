63e75f080bcdf3d4b8e0413d20a1cb8416faba48
mjason <mjasoncuba@gmail.com>
2023-03-22 18:27:00 +0100

    Added parametes

---
+++ code/data.py
@@ -0,0 +1,51 @@
+# data preprocesing funtions here
+
+from random import sample
+import pandas as pd
+from code.parameters import PARAMS
+
+
+def load_data(data_path: str):
+    data = pd.read_csv(data_path)
+    return data
+
+
+def save_data(data: pd.Series):
+    file_path_train = PARAMS["data_train"]
+    file_path_test = PARAMS["data_test"]
+    target_name = PARAMS["DATA_TARGET_COLUMN_NAME"]
+    percent = 1.0 - PARAMS["data_percent"]
+
+    data_true = data.query(target_name + "==1")
+    data_false = data.query(target_name + "==0")
+
+    train_true_ids = sample(data_true.index.tolist(), int(len(data_true) * percent))
+    train_false_ids = sample(data_false.index.tolist(), int(len(data_false) * percent))
+
+    data_train = pd.concat(
+        [data_true.loc[train_true_ids], data_false.loc[train_false_ids]], axis=0
+    )
+    data_train.to_csv(file_path_train, index=False)
+
+    del data_train
+
+    data_true = data_true.drop(train_true_ids)
+    data_false = data_false.drop(train_false_ids)
+
+    data_test = pd.concat([data_true, data_false], axis=0)
+    data_test.to_csv(file_path_test, index=False)
+
+    del data_test
+
+    return file_path_train, file_path_test
+
+# ----------------------------------------------------------------
+
+DATA_PIPELINE = [load_data, save_data]
+
+
+def processData():
+    curr_value = PARAMS["DATA_PATH"]
+
+    for f in DATA_PIPELINE:
+        curr_value = f(curr_value)
---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from code.parameters import check_params
+from code.data import processData
+
+if __name__ == '__main__':
+
+    # PARAMETERS
+    if check_params(arg=sys.argv[1:]) == 0:
+        exit(0)
+
+    # Seed
+    # setSeed()
+    
+    # DATA PIPELINE
+    processData()
+
+    # Model train 
+    # trainModel()
